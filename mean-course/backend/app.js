e60c3587837ba200d2ff6c5041e0e2b16c0a5832
Erika <erikacardon@gmail.com>
2023-06-20 21:53:30 -0500

    End week 10

---
+++ mean-course/backend/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow_Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).json({
+        message: 'Post added successfully'
+    });
+});
+
+app.get('/api/posts', (req, res, next) => {
+    const posts = [
+        { 
+            id: 'fasdfiadfk', 
+            title: 'First server-side post', 
+            content: 'This is coming from the server' 
+        },
+        { 
+            id: 'ghjfghjjghfj', 
+            title: 'Second server-side post', 
+            content: 'This is coming from the server also' 
+        },
+    ];
+    res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: posts
+    });
+});
+
+module.exports = app;
